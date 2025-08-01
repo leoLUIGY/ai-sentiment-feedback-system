a6fef8d7bf4aa104ece23bf64936e6a2e752fe7a
leoLUIGY <luigyleonidas@gmail.com>
2025-07-31 23:47:55 -0300

    initial structure

---
+++ backend/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <form method="POST" action="insert_review.php">
+        <label for="feedback">Escreva a sua avaliação</label><br>
+        <textarea name="feedback" rows="5" cols="50" required>     
+        </textarea><br>
+        <input type="submit" value="Enviar">
+    </main>
+</form>
+</body>
+</html>
+
