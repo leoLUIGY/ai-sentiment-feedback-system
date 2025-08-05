4f743b628f27690c054a8d822774f4f32b4f3f54
leoLUIGY <luigyleonidas@gmail.com>
2025-08-05 00:00:42 -0300

    adding css class and h2 tag

--- backend/form.php
+++ backend/form.php
@@ -4,16 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <main>
-        <form method="POST" action="insert_review.php">
-        <label for="feedback">Escreva a sua avaliação</label><br>
-        <textarea name="feedback" rows="5" cols="50" required>     
-        </textarea><br>
-        <input type="submit" value="Enviar">
+    <main class="form-container">
+        <form method="POST" action="insert_review.php" class="review-form">
+            <h2>Sua Avaliação</h2>
+            <label for="feedback">Escreva a sua avaliação</label><br>
+            <textarea name="feedback" rows="5" cols="50" required>     
+            </textarea><br>
+            <input type="submit" value="Enviar">
+        </form>
     </main>
-</form>
+
 </body>
 </html>
 
