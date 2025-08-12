a7871aa53011873cdf8c6e18b0f55763dda6cb62
leoLUIGY <luigyleonidas@gmail.com>
2025-08-12 00:00:47 -0300

    setting permissions

--- backend/form.php
+++ backend/form.php
@@ -8,11 +8,21 @@
 </head>
 <body>
     <main class="form-container">
-        <form method="POST" action="insert_review.php" class="review-form">
+        <form method="POST" action="review.php" class="review-form">
             <h2>Sua Avaliação</h2>
             <label for="feedback">Escreva a sua avaliação</label><br>
             <textarea name="feedback" rows="5" cols="50" required>     
             </textarea><br>
+
+            <label for="nota">Nota (1 = ruim, 5 = excelente)</label>
+            <select name="nota" required>
+                <option value="">Selecione</option>
+                <option value="1">1 - Terrivel</option>
+                <option value="2">2 - Ruim</option>
+                <option value="3">3 - Neutro</option>
+                <option value="4">4 - Bom</option>
+                <option value="5">5 - Excelente</option>
+            </select><br>
             <input type="submit" value="Enviar">
         </form>
     </main>
